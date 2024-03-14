bf24dea6187b8af95b6afe49ab042db611450f98
Daniël de Kok <me@danieldk.eu>
2024-03-14 15:43:08 +0000

    Unmasked tokens indices
    We should add a custom op for this, but this is a small reference
    reference implementation.

--- oxidized-transformers/src/layers/attention/mask.rs
+++ oxidized-transformers/src/layers/attention/mask.rs
@@ -1,4 +1,5 @@
-use candle_core::Tensor;
+use candle_core::{DType, Tensor, D};
+use candle_nn::seq;
 use snafu::{ensure, ResultExt, Snafu};
 
 /// Errors for attention masks.
@@ -44,5 +45,45 @@ impl AttentionMask {
             bool_mask: Tensor::cat(&[&self.bool_mask, &other.bool_mask], 1)
                 .context(ConcatMasksSnafu)?,
         })
+    }
+
+    /// Get the indices of pieces that are not masked.
+    pub fn unmasked_indices(&self) -> Result<Tensor, candle_core::Error> {
+        let flat_mask = self.bool_mask.flatten_all()?.to_dtype(DType::I64)?;
+
+        let piece_indices = Tensor::arange(0, flat_mask.dim(0)? as i64, self.bool_mask.device())?
+            .mul(&flat_mask)?;
+
+        let target_indices = flat_mask
+            .to_dtype(DType::F32)?
+            .cumsum(D::Minus1)?
+            .to_dtype(DType::I64)?
+            .broadcast_sub(&Tensor::try_from(1i64)?)?
+            .mul(&flat_mask)?;
+
+        Tensor::zeros(
+            flat_mask.sum_all()?.to_scalar::<i64>()? as usize,
+            DType::I64,
+            self.bool_mask.device(),
+        )?
+        .index_add(&target_indices, &piece_indices, 0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use candle_core::{Device, Tensor};
+
+    use crate::layers::attention::AttentionMask;
+
+    #[test]
+    fn test_indices() {
+        let mask = AttentionMask::new(
+            Tensor::from_vec(vec![0u32, 1, 1, 1, 0, 1], &[2, 3], &Device::Cpu).unwrap(),
+        )
+        .unwrap();
+        let indices = mask.unmasked_indices().unwrap();
+        eprintln!("indices: {:?}", indices);
+        assert!(false);
     }
 }
